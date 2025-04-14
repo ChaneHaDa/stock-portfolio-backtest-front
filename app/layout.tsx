09e3e72fe05dfb64aab21b9857a835b3d439af24
ChaneHaDa <chane.ha.da@gmail.com>
2025-04-14 22:58:28 +0900

    feat: 레이아웃 중앙정렬 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <body className="bg-gray-100 min-h-screen flex flex-col">
         <Header />
         {/* main 영역이 flex 컨테이너가 되어 내부 콘텐츠를 중앙 정렬하도록 수정 */}
-        <main className="p-8 flex-grow flex flex-col items-center justify-center">{children}</main> 
+        <main className="p-8 flex-grow flex flex-col items-center">{children}</main> 
         {/* 푸터가 항상 하단에 고정되도록 mt-auto 사용 */}
         <footer className="bg-gray-800 text-white text-center p-4 mt-auto flex-shrink-0"> 
           <p>© 2025 Stock Helper</p>
