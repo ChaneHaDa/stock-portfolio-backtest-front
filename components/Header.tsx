1405c38b0fe40618b0e952cf2aeac10eaf00f5d4
ChaneHaDa <chane.ha.da@gmail.com>
2025-09-30 11:29:56 +0900

    feat: 메뉴 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,6 +26,7 @@ const Header = () => {
     { href: "/", label: "홈", showWhenAuth: "both" },
     { href: "/portfolio", label: "내 포트폴리오", showWhenAuth: "auth" },
     { href: "/backtest", label: "백테스트", showWhenAuth: "both" },
+    { href: "/stocks", label: "주식 정보", showWhenAuth: "both" },
   ];
 
   return (
